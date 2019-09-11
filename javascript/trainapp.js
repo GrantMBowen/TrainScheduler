476ab09cda0b5272f0f36b422e0e9e99e2a99d9f
itsgoodtobegrant <itsgoodtobegrant@gmail.com>
2019-09-10 20:26:51 -0700

    got the logic working for times

--- javascript/trainapp.js
+++ javascript/trainapp.js
@@ -1,19 +1,3 @@
-// * When adding trains, administrators should be able to submit the following:
-    
-//     * Train Name
-    
-//     * Destination 
-    
-//     * First Train Time -- in military time
-    
-//     * Frequency -- in minutes
-  
-//   * Code this app to calculate when the next train will arrive; this should be relative to the current time.
-  
-//   * Users from many different machines must be able to view same train times.
-  
-//   * Styling and theme are completely up to you. Get Creative!
-
 var firebaseConfig = {
     apiKey: "AIzaSyCmki59iOpuVhydK6X81Zf4ZWybjgHP0_8",
     authDomain: "bootcamp-database.firebaseapp.com",
@@ -23,43 +7,38 @@ var firebaseConfig = {
     messagingSenderId: "705706972876",
     appId: "1:705706972876:web:f74c5d21bbb7d43f"
   };
-  // Initialize Firebase
+  
   firebase.initializeApp(firebaseConfig);
 
-    // Create a variable to reference the database
     var database = firebase.database();
 
-    // Initial Variables (SET the first set IN FIREBASE FIRST)
-    // Note remember to create these same variables in Firebase!
     var name = "";
-    var role = "";
-    var start = "";
-    var rate = "";
+    var destination = "";
+    var firsttrain = "";
+    var frequency = "";
+    var convertedTime = moment(firsttrain, "H/mm")
 
-
-    // Click Button changes what is stored in firebase
     $("#submit-button").on("click", function(event) {
-      // Prevent the page from refreshing
+      
       event.preventDefault();
 
       // Get inputs
       name = $("#name-input").val().trim();
-      role = $("#role-input").val().trim();
-      start = $("#start-date").val().trim();
-      rate = $("#rate-input").val().trim();
+      destination = $("#destination-input").val().trim();
+      firsttrain = $('#first-train').val().trim();
+      frequency = $("#frequency-input").val().trim();
 
 
       // Change what is saved in firebase
       database.ref().push({
         name: name,
-        role: role,
-        start: start,
-        rate: rate
+        destination: destination,
+        firsttrain: firsttrain,
+        frequency: frequency
       });
     });
 
-    // Firebase is always watching for changes to the data.
-    // When changes occurs it will print them to console and html
+    
     database.ref().on("child_added", function(snapshot) {
 
       // Print the initial data to the console.
@@ -67,21 +46,53 @@ var firebaseConfig = {
 
       // Log the value of the various properties
       console.log(snapshot.val().name);
-      console.log(snapshot.val().role);
-      console.log(snapshot.val().start);
-      console.log(snapshot.val().rate);
+      console.log(snapshot.val().destination);
+      console.log(snapshot.val().firsttrain);
+      console.log(snapshot.val().frequency);
 
-      // Change the HTML
-    //   $("<tbody>").prepend("<tr><th scope='row'>" + snapshot.val().name + " </th><td> " + snapshot.val().role + "</td><td>" + snapshot.val().start + "</td><td>" + snapshot.val().rate + "</td></tr>");
-    $("#full-member-list").append("<tr><th scope='row'><span class='employee-name'>" +snapshot.val().name + 
-    "</span></th><td><span class='employee-role'>" + snapshot.val().role + 
-    "</span></td><td><span class='employee-start'>" + snapshot.val().start + 
-    "</span></td><td><span class='time-worked'>Months</span></td><td><span class='employee-rate'>" + snapshot.val().rate + 
-    "</span></td><td><span class='total-hours'>Tot</span></td></tr>");
+    
+$.when(event).done(function(){
+    var frequency = snapshot.val().frequency;
+    
+    var firsttrain = snapshot.val().firsttrain;
+    console.log("This train starts running at: " + firsttrain);
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firsttrain, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    var tFrequency = frequency;
+    console.log("Train runs every " + frequency + " minutes");
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minutes Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+   
+    $("#full-train-list").append("<tr><th scope='row'><span class='train-name'>" +snapshot.val().name + 
+    "</span></th><td><span class='train-dest'>" + snapshot.val().destination + 
+    "</span></td><td><span class='first-train'>" + snapshot.val().firsttrain + 
+    "</span></td><td><span class='train-frequency'>" + snapshot.val().frequency + 
+    " min</span></td><td><span class='until-next'>00 min</span></td></tr>");
  
+});
 
-
-      // If any errors are experienced, log them to console.
     }, function(errorObject) {
       console.log("The read failed: " + errorObject.code);
     });
